b5fce81bdc18871c7072c348d3fac350d58dadfe
Pjay Medina <pjay.medina@gmail.com>
2021-08-27 20:15:09 +0800

    activity 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import allReducers from "./reducers";
+import { Provider } from "react-redux";
+
+const store = createStore(
+    allReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// NOTES
+
+//STORE - GLOBALIZED STATE
+
+//ACTION - INCREMENT - a function that returns an object
+
+// const increment = () => {
+//     return {
+//         type: "INCREMENT",
+//     };
+// };
+
+// const decrement = () => {
+//     return {
+//         type: "DECREMENT",
+//     };
+// };
+
+//REDUCER
+// const counter = (state = 0, action) => {
+//     switch (action.type) {
+//         case "INCREMENT": {
+//             return state + 1;
+//         }
+//         case "DECREMENT": {
+//             return state - 1;
+//         }
+//     }
+// };
+
+// let store = createStore(counter);
+
+//Display it in the console
+// store.subscribe(() => console.log(store.getState()));
+
+//DISPATCH
+// store.dispatch(increment());
+// store.dispatch(decrement);
+// store.dispatch(decrement);
